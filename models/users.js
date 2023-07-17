411551568781774b867e1a615670c5c134461834
DENIS TSITKO <denis.tsitko@gmail.com>
2023-07-17 12:34:35 +0300

    default deleted for test

--- models/users.js
+++ models/users.js
@@ -5,18 +5,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
-    default: "Jacque Cousteau",
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    default: "Ocean explorer",
   },
   avatar: {
     type: String,
-    default:
-      "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
   },
 });
 
