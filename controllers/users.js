11415477c724bbf8dc3f972d0426a47647696d75
DENIS TSITKO <denis.tsitko@gmail.com>
2023-07-17 11:39:08 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,77 +1,77 @@
-const User = require("../models/user");
+const User = require('../models/user');
 
-const getUser = (req, res, next) => {
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'error' }));
+};
+
+module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => {
-      if (!user) {
-        console.log("User not found");
-      }
-      return res.status(200).send({ data: user });
-    })
+    .orFail()
+    .then((user) => res.status(200).send(user))
     .catch((err) => {
-      if (err.kind === "ObjectId") {
-        console.log("Wrond Id");
+      if (err.name === 'CastError') {
+        return res
+          .status(400)
+          .send({ message: 'User id is in incorrect format' });
       }
-      return next(err);
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'User not found' });
+      }
+      return res.status(500).send({ message: 'error' });
     });
 };
 
-const getUsers = (req, res, next) => {
-  User.find()
-    .then((users) => {
-      if (!users) {
-        throw new NotFoundError("No users found");
-      }
-      return res.status(200).sent({ data: users });
-    })
-    .catch((err) => next(err));
-};
-
-const createUser = (req, res, next) => {
+module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  User.create({
-    name,
-    about,
-    avatar,
-  })
-    .then((user) => {
-      res.status(201).send({
-        name: user.name,
-        about: user.about,
-        avatar: user.avatar,
-      });
-    })
+
+  User.create({ name, about, avatar })
+    .then((user) => res.status(201).send({ data: user }))
     .catch((err) => {
-      return next(err);
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({
+          message: 'Unable to create user. User data is incorrect',
+        });
+      }
+      return res.status(500).send({ message: 'error' });
     });
 };
 
-const updateUser = (req, res, next) => {
+module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
-  return User.findByIdAndUpdate(req.user._id, {
-    name,
-    about,
-  })
-    .then((user) => {
-      return res.status(200).send({ data: user });
-    })
+
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+    .orFail()
+    .then((user) => res.status(200).send(user))
     .catch((err) => {
-      return next(err);
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({
+          message:
+            'Unable to update user. User data is incorrect',
+        });
+      }
+      return res.status(500).send({ message: 'error' });
     });
 };
 
-const updateAvatar = (req, res, next) => {
+module.exports.updaAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, {
-    avatar,
-  })
-    .then((user) => {
-      return res.status(200).send({ data: user });
-    })
+
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    .orFail()
+    .then((user) => res.status(200).send(user))
     .catch((err) => {
-      return next(err);
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({
+          message: 'Unable to update avatar',
+        });
+      }
+      return res.status(500).send({ message: 'error' });
     });
 };
+
+
 
 module.exports = {
   getUsers,
