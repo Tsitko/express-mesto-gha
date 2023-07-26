421d32de7111e8448bd7f715a1851efd4f98bf32
DENIS TSITKO <denis.tsitko@gmail.com>
2023-07-26 12:03:54 +0300

    bugfix user routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors, celebrate, Joi } = require('celebrate');
+const regEx = require('./utils/constants');
 const { login, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
@@ -34,6 +35,7 @@ app.post(
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(regEx),
       email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
