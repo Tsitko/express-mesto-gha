8fabccffe7e070ecd3b48102d9630af879044e63
DENIS TSITKO <denis.tsitko@gmail.com>
2023-07-26 11:49:45 +0300

    bugfixing signup-signin

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { errors, celebrate, Joi } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
@@ -15,11 +16,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
+
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  createUser,
+);
 app.use('*', require('./routes/404'));
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.use(errors());
 
+app.use((err, _, res, next) => {
+  const { statusCode = 500, message = 'Server error' } = err;
+  res.status(statusCode).send({ message });
+  next();
+});
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
