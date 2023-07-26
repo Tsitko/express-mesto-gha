d48052c18213eeb2ecc9dd1eb87edef748127d2c
DENIS TSITKO <denis.tsitko@gmail.com>
2023-07-26 13:54:21 +0300

    bugfixing

--- app.js
+++ app.js
@@ -1,54 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { errors, celebrate, Joi } = require('celebrate');
-const regEx = require('./utils/constants');
-const { login, createUser } = require('./controllers/users');
+const { errors } = require('celebrate');
+const router = require('./routes');
+const selectErrors = require('./middlewares/errors');
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
-
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
 
-app.post(
-  '/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  login,
-);
-
-app.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(regEx),
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  createUser,
-);
-app.use('*', require('./routes/404'));
+app.use(router);
 
 app.use(errors());
+app.use(selectErrors);
 
-app.use((err, _, res, next) => {
-  const { statusCode = 500, message = 'Server error' } = err;
-  res.status(statusCode).send({ message });
-  next();
+app.listen(PORT, () => {
+  console.log(`start server at port ${PORT}`);
 });
-app.listen(PORT);
