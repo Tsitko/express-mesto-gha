30978bbec6bd066f8755accd59c38b9ca62a4729
DENIS TSITKO <denis.tsitko@gmail.com>
2023-08-02 13:05:44 +0300

    cors

--- app.js
+++ app.js
@@ -13,6 +13,35 @@ const app = express();
 
 app.use(express.json());
 
+const allowedCors = [
+  'https://api.mesto-russia.nomoreparties.co',
+  'http://api.mesto-russia.nomoreparties.co',
+  'https://mesto-russia.nomoreparties.co',
+  'http://mesto-russia.nomoreparties.co',
+  'localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
+});
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
+
 app.post(
   '/signin',
   celebrate({
