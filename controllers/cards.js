efc0e085b50e0f1117fc5eac276572865328adba
DENIS TSITKO <denis.tsitko@gmail.com>
2023-08-07 15:48:39 +0300

    data fix

--- controllers/cards.js
+++ controllers/cards.js
@@ -16,7 +16,7 @@ const createCard = (req, res, next) => {
 
   return Card.create({ name, link, owner: req.user._id })
     .then((card) => {
-      res.status(201).send({ data: card });
+      res.status(201).send({ card });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
@@ -58,7 +58,7 @@ const likeCard = (req, res, next) => {
       if (!card) {
         throw new NotFoundError('Нет такой карточки');
       }
-      return res.status(200).send({ data: card });
+      return res.status(200).send({ card });
     })
     .catch((err) => {
       if (err.kind === 'ObjectId') {
@@ -78,7 +78,7 @@ const dislikeCard = (req, res, next) => {
       if (!card) {
         throw new NotFoundError('Нет такой карточки');
       }
-      return res.status(200).send({ data: card });
+      return res.status(200).send({ card });
     })
     .catch((err) => {
       if (err.kind === 'ObjectId') {
