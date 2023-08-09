208ddbd49a2075e80d40a97cc592650dfc841737
DENIS TSITKO <denis.tsitko@gmail.com>
2023-08-09 10:04:59 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,22 @@
 const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('./errors/UnauthorizedError');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+const { JWT_SECRET = 'dev-key' } = process.env;
 
 module.exports = (req, _, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Ошика авторизации');
+    throw new UnauthorizedError('Authorization is not working as expected');
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'super-strong-secret');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    next(new UnauthorizedError('Необходима авторизация'));
+    next(new UnauthorizedError('Authorization required'));
   }
 
   req.user = payload;
